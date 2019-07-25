71499534b27d525326a01b68a33a4cc729f9a453
Minsung <LGCNS@DESKTOP-V3V550V.lgcns.com>
2019-07-25 11:21:47 +0900

    #1.1 done

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("hello");
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import { Server } from "tls";
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hi"
+    }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+server.start({port:PORT}), console.log(`server running on port http://localhost:${PORT}`);
